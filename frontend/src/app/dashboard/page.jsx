3877fa67e0ff3168750b5c9a5fa273079ed03dc4
Satenik <saten914@gmail.com>
2025-03-21 19:16:35 +0100

    feature: Definimos componente Dashboard y añadimos estados Dinamicos

--- frontend/src/app/dashboard/page.jsx
+++ frontend/src/app/dashboard/page.jsx
@@ -3,3 +3,10 @@
 import Link from "next/link";
 import { useState } from "react";
 
+export default function Dashboard() {
+    const [reports, setReports] = useState(0);
+    const [unansweredQuestions, setUnansweredQuestions] = useState(0);
+    const [active, setActiveUsers] = useState(0);
+
+    
+}
