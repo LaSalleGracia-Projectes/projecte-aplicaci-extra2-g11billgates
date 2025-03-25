5b20d3b418c6393928a58909768d9a5f4378003a
Satenik <saten914@gmail.com>
2025-03-25 17:17:40 +0100

    feature: Tarjetas de Reportes creadas

--- frontend/src/app/reports/page.jsx
+++ frontend/src/app/reports/page.jsx
@@ -8,4 +8,20 @@ export default function Reports() {
     const [registers, setRegisters] = useState(0);
     const [ totalUsers, setTotalUsers ] = useState(0);
     const [ matches, setMatches ] = useState(0);
+
+ return (
+         <div className="min-h-screen bg-gray-100 text-gray-900">
+             <Header/>
+ 
+             <main className="p-6 mt-20">
+                 <h2 className="text-2xl font-semibold mb-6">Reportes y Métricas</h2>
+ 
+                 <div className="text-white grid grid-cols-1 md:grid-cols-3 gap-8">
+                     <StatCard title="Nuevos Registros" value={reports} />
+                     <StatCard title="Usuarios Totales" value={unansweredQuestions} />
+                     <StatCard title="Matches" value={activeUsers} />
+                 </div>
+             </main>
+         </div>
+     );  
 }
