a1b9526f5f47ced3a6c571456b905a3086a15588
Satenik <saten914@gmail.com>
2025-03-25 12:46:23 +0100

    fix: Componente para mostrar estadísticas

--- frontend/src/app/components/dashboard/StatCard.jsx
+++ frontend/src/app/components/dashboard/StatCard.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-default export StatCard ({ title,value }) {
+default export function StatCard ({ title,value }) {
     return (
         <div className="bg-gray-800 p-6 rounded-lg shadow-md text-center">
             <h3 className="text-lg font-medium">{title}</h3>
