737b5d921ac85bab154ce14f961922f089089635
Satenik <saten914@gmail.com>
2025-03-25 16:22:00 +0100

    feature: Definimos los estados del componente

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -3,3 +3,10 @@
 import { useState, useEffect } from "react";
 import { useRouter} from "next/navigation";
 
+export default function Login () {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe,setRememberMe] = useState("");
+  const router = useRouter();
+
+}
