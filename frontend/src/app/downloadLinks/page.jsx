8cee3285b4c7a141e7179da8e6abce370815c68d
Satenik <saten914@gmail.com>
2025-04-03 11:36:05 +0200

    feat: crear estructura base de la página de enlaces de descarga

---
+++ frontend/src/app/downloadLinks/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Header from "@/app/components/Header";
+
+export default function DownloadLinks() {
+    return (
+        <div className="min-h-screen bg-gray-100 text-gray-900">
+            <Header />
+            <main className="p-6 mt-20">
+                <h2 className="text-2xl font-bold mb-6 border-b-2 pb-2">Enlaces de Descarga</h2>
+            </main>
+        </div>
+    );
+}
