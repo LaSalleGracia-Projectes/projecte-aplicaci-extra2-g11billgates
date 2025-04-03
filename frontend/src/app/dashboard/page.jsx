450fe794b9931745100abaf693704403d63a91b1
Satenik <saten914@gmail.com>
2025-04-03 16:54:28 +0200

    feat: cambios

--- frontend/src/app/dashboard/page.jsx
+++ frontend/src/app/dashboard/page.jsx
@@ -12,7 +12,6 @@ export default function Dashboard() {
     return (
         <div className="min-h-screen bg-gray-100 text-gray-900">
             <Header/>
-
             <main className="p-6 mt-20">
                 <h2 className="text-2xl font-semibold mb-6 mb-4 border-b-2 pb-2">Bienvenido al Panel Administrativo</h2>
                 <p className="text-sm text-gray-600 mb-6">
