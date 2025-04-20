b7733e8bfa46eca27dd64d72f3f04c092aef5e5d
Satenik <saten914@gmail.com>
2025-04-20 02:20:55 +0200

    feature: Añade ruta POST /api/emails/respond al router de mensajes

--- backend/correo-api/src/routes/messageRoutes.ts
+++ backend/correo-api/src/routes/messageRoutes.ts
@@ -6,5 +6,6 @@ const router = Router();
 router.get('/new', messageController.getEmails as RequestHandler);
 router.get('/all', messageController.getAllMessages as RequestHandler);
 router.delete('/:id', messageController.deleteMessage as RequestHandler);
+router.post('/respond', messageController.respondToMessage as RequestHandler);
 
 export default router;
