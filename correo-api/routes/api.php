95dcbcfe14760eca6d4076f8e6b062c595ee2dab
Saten <158043932+SatenikAvetisyan@users.noreply.github.com>
2025-04-20 02:23:56 +0200

    Delete correo-api directory

--- correo-api/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmailsController;
-
-Route::get('/emails', [EmailsController::class, 'index']);
