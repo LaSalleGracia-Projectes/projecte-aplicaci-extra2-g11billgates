13ed943b797cacc278a3ec630bb03099bca8cbaf
Satenik <saten914@gmail.com>
2025-04-20 15:18:21 +0200

    fix: Configura conexión a MongoDB Atlas en user-api

--- backend/user-api/src/routes/userRoutes.ts
+++ backend/user-api/src/routes/userRoutes.ts
@@ -1,14 +1,1 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-export const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI as string);
-    console.log('✅ Conectado a MongoDB Atlas (usuarios)');
-  } catch (error) {
-    console.error('❌ Error al conectar a MongoDB:', error);
-    process.exit(1);
-  }
-};
