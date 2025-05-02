4b7dacbb47d718738c2568e20fb51eb9ba70de64
Satenik <saten914@gmail.com>
2025-05-02 18:59:33 +0200

    feat: Implementar logout con JWT usando blacklist de tokens

--- backend/user-api/src/routes/userRoutes.ts
+++ backend/user-api/src/routes/userRoutes.ts
@@ -1,5 +1,6 @@
 import { Router, RequestHandler } from 'express';
 import * as userController from '../controllers/userController';
+import * as authController from '../controllers/authController';
 import  { verifyToken }  from '../middleware/verifyToken';
 import { verifyAdmin } from '../middleware/verifyAdmin';
 import { warnUser, banUser } from '../controllers/userModerationController';
@@ -10,7 +11,8 @@ router.get('/', verifyToken, verifyAdmin, userController.getAllUsers as RequestH
 router.post('/', verifyToken, verifyAdmin, userController.createUser as RequestHandler);
 router.put('/:id', verifyToken, verifyAdmin, userController.updateUser as RequestHandler);
 router.delete('/:id', verifyToken,verifyAdmin, userController.deleteUser as RequestHandler);
-router.post('/login', userController.loginUser as RequestHandler);
+router.post('/login', authController.loginUser as RequestHandler);
+router.post('/logout', authController.logout as RequestHandler);
 router.post('/users/:userId/warn', verifyAdmin, verifyToken, warnUser as unknown as RequestHandler);
 router.post('/users/:userId/ban', verifyAdmin, verifyToken, banUser as unknown as RequestHandler);
 
