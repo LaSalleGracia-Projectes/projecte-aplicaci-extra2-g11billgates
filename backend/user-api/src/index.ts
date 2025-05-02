758fc9966b853e3ca8c61e35452e58d34c96253b
Satenik <saten914@gmail.com>
2025-05-02 20:15:43 +0200

    fix: Errores en rutas

--- backend/user-api/src/index.ts
+++ backend/user-api/src/index.ts
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import { connectDB } from './config/db';
 import userRoutes from './routes/userRoutes';
-import statsRoutes from './routes/stats.routes';
 
 dotenv.config();
 
@@ -16,7 +15,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
-app.use('/api', statsRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor user-api corriendo en http://localhost:${PORT}`);
