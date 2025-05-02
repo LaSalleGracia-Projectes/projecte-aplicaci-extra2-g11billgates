c8ba8fcbcbfec6bb0ea792db0b92f9e9bfc707af
Satenik <saten914@gmail.com>
2025-05-02 21:41:21 +0200

    feat: middleware añadido para proteger las rutas de correo-api

--- backend/correo-api/src/routes/messageRoutes.ts
+++ backend/correo-api/src/routes/messageRoutes.ts
@@ -1,11 +1,13 @@
 import { Router, RequestHandler } from 'express';
 import * as messageController from '../controllers/messageController';
+import  { verifyToken }  from '../middleware/verifyToken';
+import { verifyAdmin } from '../middleware/verifyAdmin';
 
 const router = Router();
 
-router.get('/new', messageController.getEmails as unknown as RequestHandler);
-router.get('/all', messageController.getAllMessages as unknown as RequestHandler);
-router.delete('/:id', messageController.deleteMessage as unknown as RequestHandler);
-router.post('/respond', messageController.respondToMessage as unknown as RequestHandler);
+router.get('/new', verifyToken, verifyAdmin, messageController.getEmails as unknown as RequestHandler);
+router.get('/all', verifyToken, verifyAdmin, messageController.getAllMessages as unknown as RequestHandler);
+router.delete('/:id', verifyToken, verifyAdmin, messageController.deleteMessage as unknown as RequestHandler);
+router.post('/respond', verifyToken, verifyAdmin, messageController.respondToMessage as unknown as RequestHandler);
 
 export default router;
