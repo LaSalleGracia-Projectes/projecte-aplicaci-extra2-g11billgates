eab1858da1120589482d8bd18df6691d2f247bb5
Satenik <saten914@gmail.com>
2025-05-04 21:30:31 +0200

    feat: añadir los ficheros authcontroller, user.interface, user.model para autentificación

---
+++ backend/correo-api/src/interfaces/user.interface.ts
@@ -0,0 +1,12 @@
+export interface IUser {
+    _id?: string;
+    username: string;
+    password: string;
+    role: 'Administrador' | 'Usuario';
+    status: 'Activo' | 'Inactivo';
+    reported?: boolean;
+    reason?: 'Comportamiento tóxico' | 'Vocabulario inadecuado' | 'Otro';
+    warnings: number;
+    banned: boolean;
+  }
+  
---
+++ backend/correo-api/src/models/user.model.ts
@@ -0,0 +1,15 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import { IUser } from '../interfaces/user.interface';
+
+const UserSchema: Schema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['Administrador', 'Usuario'], default: 'Usuario' },
+  status: { type: String, enum: ['Activo', 'Inactivo'], default: 'Activo' },
+  warnings: { type: Number, default: 0 },
+  banned: { type: Boolean, default: false },
+  reported: { type: Boolean, default: false},
+  reason: { type: String, enum: ['Comportamiento tóxico','Vocabulario inadecuado','Otro'], default: 'Otro'}
+});
+
+export default mongoose.model<IUser & Document>('User', UserSchema);
